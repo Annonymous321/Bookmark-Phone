1c52a905cdca66078dcb661b1b73b8628a852851
Annonymous321 <93349281+Annonymous321@users.noreply.github.com>
2024-01-23 23:54:45 -0500

    Delete default apps/Calculator directory

--- default apps/Calculator/buttons/0-9/script.js
+++
@@ -1,8 +0,0 @@
-const buttons = document.querySelectorAll('.buttons button');
-
-buttons.forEach(button => {
-  button.addEventListener('click', function() {
-    const currentNumber = document.getElementById('display').value;
-    document.getElementById('display').value = currentNumber + this.textContent;
-  });
-});
