db401fb3f1e51e75a8bd3282495fbc83a1efb80e
Annonymous321 <93349281+Annonymous321@users.noreply.github.com>
2024-01-23 20:12:02 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
+function openApp(appId) {
+  location.href = appId + ".html";
+}
 
+const appIcons = document.querySelectorAll(".app-icon");
+appIcons.forEach(icon => icon.addEventListener("click", () => openApp(icon.textContent.toLowerCase())));
